9320d6afa1e215a1df23a2f339b7959cd53f99b8
andryabreu81 <andryabreu@gmail.com>
2026-09-26 14:48:36 -0400

    implementacion de libreria sweetalert2

--- src/rols/rols.service.ts
+++ src/rols/rols.service.ts
@@ -8,46 +8,46 @@ import { RoleEntity } from './rols.entity'
 @Injectable()
 export class RolsService {
   constructor(
-      @InjectRepository(RoleEntity)
-      private readonly rolsRepository: Repository<RoleEntity>,
-    ) {}
-  
-    async getRoles(): Promise<RoleEntity[]> {
-      console.log('here service');
-      return await this.rolsRepository.find({
-        relations:['users']
-      });
-    }
+    @InjectRepository(RoleEntity)
+    private readonly rolsRepository: Repository<RoleEntity>,
+  ) { }
 
-    // Obtener un rol por su ID
-    async findRol(rolId: string): Promise<RoleEntity | null> {
-      return await this.rolsRepository.findOne({ 
-        where: { id: rolId },
-        relations:['users'] 
-      });
-    }
+  async getRoles(): Promise<RoleEntity[]> {
+    //console.log('here service');
+    return await this.rolsRepository.find({
+      relations: ['users']
+    });
+  }
 
-    // Agregar un rol
-    async addRol(role_code: string, descripcion: string, active?: number): Promise<RoleEntity> {
-      const newRol = this.rolsRepository.create({ role_code, descripcion, active });
-      return await this.rolsRepository.save(newRol);
-    }
+  // Obtener un rol por su ID
+  async findRol(rolId: string): Promise<RoleEntity | null> {
+    return await this.rolsRepository.findOne({
+      where: { id: rolId },
+      relations: ['users']
+    });
+  }
 
-    // Actualizar un rol especifico
-    async updateRol(
-      rolId: string,
-      updateData: { role_code?: string; descripcion?: string; active?: number; }
-    ): Promise<RoleEntity | null> {
-      await this.rolsRepository.update(rolId, updateData);
-      return this.findRol(rolId);
-    }
+  // Agregar un rol
+  async addRol(role_code: string, descripcion: string, active?: number): Promise<RoleEntity> {
+    const newRol = this.rolsRepository.create({ role_code, descripcion, active });
+    return await this.rolsRepository.save(newRol);
+  }
 
-    // Eliminar un rol especifico
-    async deleteRol(rolId: string): Promise<boolean> {
-      if (!rolId) {
-        throw new Error('El ID del rol es requerido para eliminar.');
-      }
-      const result = await this.rolsRepository.delete(rolId);
-      return (result.affected ?? 0) > 0;
+  // Actualizar un rol especifico
+  async updateRol(
+    rolId: string,
+    updateData: { role_code?: string; descripcion?: string; active?: number; }
+  ): Promise<RoleEntity | null> {
+    await this.rolsRepository.update(rolId, updateData);
+    return this.findRol(rolId);
+  }
+
+  // Eliminar un rol especifico
+  async deleteRol(rolId: string): Promise<boolean> {
+    if (!rolId) {
+      throw new Error('El ID del rol es requerido para eliminar.');
     }
+    const result = await this.rolsRepository.delete(rolId);
+    return (result.affected ?? 0) > 0;
+  }
 }
